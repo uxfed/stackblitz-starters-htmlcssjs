2ccd952093b1fca0a2148c56ec113a230a0d097d
uxfed <pat@uxfed.com>
2024-05-20 11:52:28 -0400

    JSX two buttons

--- app.js
+++ app.js
@@ -6,23 +6,42 @@ console.log('Hello, Pat!!!');
 
 const rootNode = document.getElementById("app");
 const root = ReactDOM.createRoot(rootNode);
+let counterName = "One";
 root.render(React.createElement(App));
 
 function App() {
+    const counterOne = <Counter name={counterName} />;
+    const counterTwo = <Counter2 name={counterName} />;
     return <section>
         <h1>Counters</h1>
         <section>
-            <Counter />
+            {counterName === "One" ? counterOne : counterTwo}
         </section>
     </section>;
 }
 
-function Counter() {
+function Counter( {name} ) {
     return  <article>
-        <h2>Counter</h2>
+        <h2>Counter {name}</h2>
         <p>You clicked 1 times</p>
         <button className="button">
             Click me
         </button>
     </article>;
+}
+
+function Counter2( {name} ) {
+    return  <article>
+        <h2>Counter {name}</h2>
+        <p>Times clicked... </p>
+        <button className="button">
+            Click me
+        </button>
+    </article>;
+}
+
+function rerender() {
+    console.log("Rerender...");
+    counterName = "Two";
+    root.render(React.createElement(App));
 }
